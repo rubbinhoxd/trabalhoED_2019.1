fb8176660b2f8c261dcaed428427a162335b057f
rubbinhoxd <39568251+rubbinhoxd@users.noreply.github.com>
2019-05-03 13:39:53 -0300

    Update bubble.c

--- Sixth_Week/bubble.c
+++ Sixth_Week/bubble.c
@@ -4,54 +4,53 @@
 
 void show(int vet[], int size){
 
-	printf("[ ");
+  printf("[ ");
 
-	for(int i = 0; i < size; i++){
-		printf("%d", vet[i]);
-		if(i < size - 1){
-			printf(" ");
-		}
-	}
-	printf("] ");
-
+  for(int i = 0; i < size; i++){
+    printf("%d", vet[i]);
+    if(i < size - 1){
+	printf(" ");
+    }
+  }
+  printf("] ");
 }
 
 void bubble_sort(int vet[], int size){
 	
-	int aux = 0;
+  int aux = 0;
 
-	for(int i = 0; i < size - 1; i++){
-		for(int j = 0; j < size - 1; j++){
-			if(vet[j] > vet[j+1]){
-				aux = vet[j];
-				vet[j] = vet[j+1];
-				vet[j+1] = aux;
-			}
-		}
-	}
+  for(int i = 0; i < size - 1; i++){
+      for(int j = 0; j < size - 1; j++){
+	    if(vet[j] > vet[j+1]){
+		aux = vet[j];
+		vet[j] = vet[j+1];
+		vet[j+1] = aux;
+	    }
+      }
+  }
 
 }
 
 int main(){
 
-	int size = 0;
+  int size = 0;
 
-	printf("Digite o tamanho do seu vetor:\n");
+  printf("Digite o tamanho do seu vetor:\n");
 
-	scanf("%d", &size);
+  scanf("%d", &size);
 
-	int vet[size];
+  int vet[size];
 
-	printf("Agora, digite os valores do seu vetor: \n");
+  printf("Agora, digite os valores do seu vetor: \n");
 
-	for(int i = 0; i< size; i++){
-		scanf("%d", &vet[i]);
-	}
+  for(int i = 0; i< size; i++){
+	scanf("%d", &vet[i]);
+  }
 
 
-	bubble_sort(vet, size);
+  bubble_sort(vet, size);
 
-	show(vet, size);
+  show(vet, size);
 
-	return 0;
+  return 0;
 }
