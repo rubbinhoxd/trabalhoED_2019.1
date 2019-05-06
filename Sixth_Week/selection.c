b6f0d5cbd32644e69f4310ae0e055d643c7ba0ef
rubbinhoxd <39568251+rubbinhoxd@users.noreply.github.com>
2019-05-06 13:53:33 -0300

    Update selection.c

--- Sixth_Week/selection.c
+++ Sixth_Week/selection.c
@@ -2,16 +2,16 @@
 
 int show(int vet[], int size){
 
-	printf("[ ");
+printf("[ ");
 
 for(int i = 0; i < size; i++){
-	printf("%d", vet[i]);
-		if(i < size - 1){
-			printf(" ");
-	}
+     printf("%d", vet[i]);
+     if(i < size - 1){
+	printf(" ");
+     }
 }
 
-	printf("] ");
+printf("] ");
 
 }
 
@@ -58,7 +58,7 @@ int main(){
 
 	show(vet, size);
 
-
+	printf("/n");	
 
 	return 0;
 }
